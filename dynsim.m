a677689d90bbb8d0354af936e17175f35d083b45
francosauvisky <francosauvisky@gmail.com>
2018-08-21 11:53:34 -0300

    working buffer

--- dynsim.m
+++ dynsim.m
@@ -7,8 +7,9 @@
 % ---- Variables:
 
 fps = 24;
-maxt = 10;
-buffsize = 20;
+buffsize = 100;
+prebuffperc = 50;
+tmax = 10;
 
 % ---- Code:
 
@@ -25,53 +26,65 @@ until l ~= -1
 
 l = l(:)';
 
-figure();
 h = imshow(ones(l));
+pause(0.001);
 
-data = zeros(l,l,buffsize); % FIFO buffer
-kbuff = 1; % start buffer index (data out)
-mbuff = 1; % end buffer index (data in)
-oldframe = 0;
+data(1:buffsize) = mat2cell(zeros(l),l(1),l(2)); % FIFO buffer
+cellzero = data(1);
+buffn = 0;
+
 frame = 0;
+fcount = 0;
+
 rawdata = -1;
+prebuff = 0;
 
 % ---- Main loop:
-
-fprintf(ctrlh,'pu'); fflush(ctrlh);
-tic;
+basetime = tic;
 do
 	% get data
 	if(rawdata == -1)
+		fprintf(ctrlh,'p'); fflush(ctrlh);
 		rawdata = fgetl(outh);
 		fclear(outh);
-	end
-
+	% clean bad data
+	elseif(length(rawdata) < l(1)*l(2))
+		rawdata = -1;
 	% add data to buffer
-	if((length(rawdata) >= l(1)*l(2)) && (mod(mbuff+1,buffsize) != kbuff))
+	elseif((length(rawdata) >= l(1)*l(2)) && buffn < buffsize)
 		rawdata((rawdata != '+') || (rawdata != '-')) = [];
 		rawdata = reshape(rawdata == '+',l);
 
-		mbuff = mod(mbuff+1,buffsize);
-		data(:,:,mbuff+1) = rawdata;
+		data{buffn + 1} = rawdata;
+		buffn = buffn + 1;
 
 		fprintf(ctrlh,'pu'); fflush(ctrlh);
 		rawdata = -1;
+
+		if(prebuff > 0)
+			prebuff = prebuff - 1;
+			if prebuff == 0
+				basetime = tic;
+				frame = 0;
+			endif
+		endif
 	endif
 
+	% prebuffer?
+	if((buffn == 0) && (prebuff == 0))
+		prebuff = round(prebuffperc*buffsize/100);
 	% plot data
-	frame = round(toc*fps) + 1;
-	if((frame > oldframe) && (kbuff != mbuff))
-		oldframe = frame;
-		set(h,'cdata',data(:,:,kbuff+1));
-		kbuff = mod(kbuff+1,buffsize);
-		pause(0.01);
+	elseif( ( (toc(basetime)*fps) > frame) && (prebuff == 0) )
+		frame = frame + 1;
+		fcount = fcount + 1;
+		set(h,'cdata',data{1});
+		pause(1e-5);
+
+		data = [data(2:end) cellzero];
+		buffn = buffn - 1;
 	endif
 
-	% clear invalid data
-	if(!isequal(rawdata, -1) && length(rawdata) < l(1)*l(2))
-		rawdata = -1;
-	endif
-until frame >= maxt*fps
+until(fcount >= tmax*fps)
 
 system(['kill -9 ' num2str(sim_pid)]);
 
