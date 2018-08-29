d741c7b4c736f572ac9d45aac1378b8d73502e8a
Leo-Souto <leonardosouto96@outlook.com>
2018-08-29 10:08:03 -0300

    Limpei o código do draw e corrigi cores no modo 5

--- src/draws.c
+++ src/draws.c
@@ -35,70 +35,41 @@ void DrawCircle(double x0, double y0, double r){
   }
   glEnd();
 }
-
-void drawPointsDemo(int width, int height){
-  GLfloat size=5.0f;
-  for(GLfloat x = 0.0f; x<=1.0f; x+=0.2f, size+=5)
-    {
-      Vertex v1 = {x, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f, 1.0f};
-      drawPoint(v1, size);
-    }
-}
-
 void drawLineSegment(Vertex v1, Vertex v2, GLfloat width) {
   glLineWidth(width);
   glBegin(GL_LINES);
+    glColor4f(v1.r, v1.g, v1.b, v1.a);
   glVertex3f(v1.x, v1.y, v1.z);
   glColor4f(v2.r, v2.g, v2.b, v2.a);
   glVertex3f(v2.x, v2.y, v2.z);
   glEnd();
 }
-
-void drawGrid(GLfloat width, GLfloat height, GLfloat grid_width){
-  //horizontal lines
-  for(float i=-height; i<height; i+=grid_width){
-    Vertex v1 = {-width, i, 0.0f, 1.0f, 1.0f, 1.0f, 0.5f};
-    Vertex v2 = {width, i, 0.0f, 1.0f, 1.0f, 1.0f, 0.5f};
-    drawLineSegment(v1, v2,grid_width);
-  }
-  //vertical lines
-  for(float i=-width; i<width; i+=grid_width){
-    Vertex v1 = {i, -height, 0.0f, 1.0f, 1.0f, 1.0f, 0.5f};
-    Vertex v2 = {i, height, 0.0f, 1.0f, 1.0f, 1.0f, 0.5f};
-    drawLineSegment(v1, v2,grid_width);
-  }
-}
-
-void DrawFrame(){
-  Vertex v1 = {-0.95f, -0.95f, 0.0f, 1.0f, 1.0f, 1.0f, 1.0f};
-  Vertex v2 = {0.95f, -0.95f, 0.0f, 1.0f, 1.0f, 1.0f, 1.0f};
-  Vertex v3 = {0.95f, 0.95f, 0.0f, 1.0f, 1.0f, 1.0f, 1.0f};
-  Vertex v4 = {-0.95f, 0.95f, 0.0f, 1.0f, 1.0f, 1.0f, 1.0f};
-}
-
-void DrawArrow(Vertex v1, Vertex v2, GLfloat width){ 
-  int i; 
-  double theta = atan2((v2.y-v1.y),(v2.x-v1.x)); 
+void DrawArrow(Vertex v1, Vertex v2, GLfloat width){
+  int i;
+  double theta = atan2((v2.y-v1.y),(v2.x-v1.x));
   double R=sqrt(pow(v2.y-v1.y,2)+pow(v2.x-v1.x,2));
-  double rho=sqrt(pow(v2.y-v1.y,2)+pow(v2.x-v1.x,2)+pow(v2.z-v1.z,2)); 
-  double headSize=0.5; 
-  
-  glPushMatrix(); 
-  glTranslatef(v1.x, v1.y, v1.z); 
-  glRotatef(theta/M_PI*180, 0.0, 0.0, 1.0f); 
-  
-  glLineWidth(width); 
-  glBegin(GL_LINES); 
-  glVertex3f(0, 0, v2.z); 
-  glColor4f(v2.r, v2.g, v2.b, v2.a); 
-  glVertex3f(R, 0, v2.z); 
-  glEnd(); 
-    
+  double rho=sqrt(pow(v2.y-v1.y,2)+pow(v2.x-v1.x,2)+pow(v2.z-v1.z,2));
+  double headSize=0.5;
+
+  glPushMatrix();
+  glTranslatef(v1.x, v1.y, v1.z);
+  glRotatef(theta/M_PI*180, 0.0, 0.0, 1.0f);
+
+  glLineWidth(width);
+  glBegin(GL_LINES);
+  glColor4f(v2.r, v2.g, v2.b, v2.a);
+  glVertex3f(0, 0, v2.z);
+  glColor4f(v2.r, v2.g, v2.b, v2.a);
+  glVertex3f(R, 0, v2.z);
+  glEnd();
+
   glBegin(GL_TRIANGLES);
   glColor4f(v2.r, v2.g, v2.b, v2.a);
-  glVertex3f(R-headSize*R, -0.7*headSize*rho, v2.z); 
-  glVertex3f(R, 0, v2.z); 
-  glVertex3f(R-headSize*R, 0.7*headSize*rho, v2.z); 
-  glEnd(); 
-  glPopMatrix(); 
-} 
+  glVertex3f(R-headSize*R, -0.7*headSize*rho, v2.z);
+  glColor4f(v2.r, v2.g, v2.b, v2.a);
+  glVertex3f(R, 0, v2.z);
+  glColor4f(v2.r, v2.g, v2.b, v2.a);
+  glVertex3f(R-headSize*R, 0.7*headSize*rho, v2.z);
+  glEnd();
+  glPopMatrix();
+}
