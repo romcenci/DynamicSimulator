2064bdd5fc997b0b1fd46563589d591a725c67d2
romcenci <romcenci@gmail.com>
2018-09-09 16:50:34 -0300

    Script que gera o arquivo de cores

---
+++ ColorFileGen.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+qtd=$(yad --title="ColorFileGen" \
+	  --width=100 \
+	  --text="Digite o número de cores:" \
+	  --entry)
+
+if [[ $qtd =~ ^-?[0-9]+$ ]]; then
+    for (( i=1; i<=$qtd; i++ )); do
+	fields+='--field=Color'$i':CLR ';
+    done;
+
+    yad --title="ColorFileGen" \
+	--width=100 \
+	--form \
+	$fields \
+	--button="Cancel:1" \
+	--button="Done:0" \
+	--separator="\n" \
+	| sed -e 's/#/0x/g' > color.txt
+else
+    echo "Digite um número de cores válido."
+fi
